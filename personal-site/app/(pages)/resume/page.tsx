ed3c84aba4382753af1be4fcdc1d8fbf2d0c6138
olincb <bailson.jr@gmail.com>
2025-01-24 22:52:12 -0500

    rename resume file

--- personal-site/app/(pages)/resume/page.tsx
+++ personal-site/app/(pages)/resume/page.tsx
@@ -4,8 +4,8 @@ export default function Resume() {
   return (
     <Stack>
       <h1>Resume</h1>
-      <p>Download <a href='/resume.pdf' target="_blank">here</a>.</p>
-      <embed src="/resume.pdf" type="application/pdf" width="900" height="1200" />
+      <p>Download <a href='/chris-olin-resume.pdf' target="_blank">here</a>.</p>
+      <embed src="/chris-olin-resume.pdf" type="application/pdf" width="900" height="1200" />
     </Stack>
   )
 }
